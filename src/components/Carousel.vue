c4556d66c8f56bc7e77f00dcd950e555575f1115
mthdht <mthdht@gmail.com>
2023-11-20 11:38:01 +0100

    made transition effect for slide to enter and leave

--- src/components/Carousel.vue
+++ src/components/Carousel.vue
@@ -1,5 +1,5 @@
 <template>
-    <section class="pl-carousel relative">
+    <section class="pl-carousel relative overflow-hidden">
         <slot></slot>
 
         <div @click="prev" class="pl-carousel-prev-slide absolute left-0 top-0 h-full w-16 flex items-center justify-center">
@@ -16,15 +16,19 @@ import { ref, useSlots, provide, } from 'vue'
 import { ChevronRightIcon, ChevronLeftIcon } from '@heroicons/vue/24/solid'
 
 const currentSlide = ref(0)
+const direction = ref()
 const slots = useSlots().default()
 
 provide('currentSlide', currentSlide)
+provide('direction', direction)
 
 function prev() {
+    direction.value = 'left'
     currentSlide.value == 0 ? currentSlide.value = slots.length - 1 : currentSlide.value--
 }
 
 function next() {
+    direction.value = 'right'
     currentSlide.value == slots.length - 1  ? currentSlide.value = 0 : currentSlide.value++
 }
 
--- src/components/Slide.vue
+++ src/components/Slide.vue
@@ -1,19 +1,95 @@
 <template>
-    <div class="pl-carousel-slide h-56" v-show="visible">
-        <slot></slot>
-    </div>
+    <transition :name="transition">
+        <div class="pl-carousel-slide h-56 relative" v-show="visible">
+            <slot></slot>
+        </div>
+    </transition>
 </template>
 
 <script setup>
-import { inject, computed, } from 'vue'
+import { inject, computed, onMounted } from 'vue'
 
 const currentSlide = inject('currentSlide')
+const direction = inject('direction')
+
 
 const props = defineProps({
-    index: Number
+    index: Number,
+    direction: String
 })
 
 const visible = computed(() => {
     return props.index == currentSlide.value
 })
+
+const transition = computed(() => {
+    return 'slide-' + direction.value
+})
+
 </script>
+
+<style scoped>
+
+.slide-left-enter-active {
+    animation: slideLeftEnter 1s ease-in-out;
+}
+
+.slide-left-leave-active {
+    position: absolute;
+    top: 0;
+    bottom: 0;
+    left: 0;
+    right: 0;
+    animation: slideLeftLeave 1s ease-in-out;
+}
+
+.slide-right-enter-active {
+    animation: slideRightEnter 1s ease-in-out;
+}
+
+.slide-right-leave-active {
+    position: absolute;
+    top: 0;
+    bottom: 0;
+    left: 0;
+    right: 0;
+    animation: slideRightLeave 1s ease-in-out;
+}
+
+
+@keyframes slideLeftEnter {
+    from {
+        transform: translateX(-100%);
+    }
+    to {
+        transform: translateX(0);
+    }
+}
+
+@keyframes slideLeftLeave {
+    from {
+        transform: translateX(0);
+    }
+    to {
+        transform: translateX(100%);
+    }
+}
+
+@keyframes slideRightEnter {
+    from {
+        transform: translateX(100%);
+    }
+    to {
+        transform: translateX(0);
+    }
+}
+
+@keyframes slideRightLeave {
+    from {
+        transform: translateX(0);
+    }
+    to {
+        transform: translateX(-100%);
+    }
+}
+</style>
